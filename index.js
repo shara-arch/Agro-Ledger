293bc0fa2e872441b29e7a673673ab195b936288
Sharon Moegi <sharamoegi@gmail.com>
2026-04-16 11:31:56 +0300

    Check if user has logged in

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 //Login function
-function handleLogin(maxAttempt, userName, password) {  
-    
+function handleLogin(maxAttempt, correctUser, correctpass) {  
+    let attempts = 0;
+
+    return function login() {
+        const userName = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+    }
+    //Check if already logged in 
+    if(localStorage.getItem("isLoggedIn") === "true"){
+        window.location.href = "index.html";
+    }
 }
