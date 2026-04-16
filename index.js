97fc3ee1adc035fd668537dd5a1d92f5115cecb2
Sharon Moegi <sharamoegi@gmail.com>
2026-04-16 11:41:50 +0300

    authenticate username and password

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 //Login function
-function handleLogin(maxAttempt, correctUser, correctpass) {  
+function handleLogin(maxAttempts, correctUser, correctPass) {  
     let attempts = 0;
 
     return function login() {
         const userName = document.getElementById("username").value;
         const password = document.getElementById("password").value;
-    }
+    
     //Check if already logged in 
     if(localStorage.getItem("isLoggedIn") === "true"){
         window.location.href = "index.html";
     }
+    //Authenticate username and password
+    if(username === correctUser && password === correctPass) {
+        localStorage.setItem("isLoggedIn", "true");
+        window.location.href = "index.html";
+    } else {
+        attempts++;
+        if (attempts < maxAttempts) {
+            alert("Incorrect credentials. Attempts left:" + (maxAttempts - attempts));
+        } else {
+            alert("Too many Attempts! Access denied.")
+            //disble login button
+            document.getElementById("login-btn").disabled = true;
+        }
+    }
+ };
 }
